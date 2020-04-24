029fa3fe2ce7d275822cb400b73217c80e8f5bb9
mRSun15 <3150105645@zju.edu.cn>
2020-04-23 23:17:56 -0400

    modify data script

--- data/geneData.py
+++ data/geneData.py
@@ -10,7 +10,7 @@ mid_point = int(width/2)
 if mode == 1:
     lightning_point = 1
 elif mode == 2:
-    lightning_point = 2
+    lightning_point = 1
 else:
     lightning_point = 5
 lightning_width = int(width/(lightning_point+1))
@@ -21,14 +21,14 @@ with open(output_file, 'w') as f:
     if mode == 1:
         f.write("0 "+str(mid_point)+"\n")
         f.write("1\n");
-        f.write("0 "+str(mid_point)+"\n")
+        f.write(str(width)+" "+str(mid_point)+"\n")
     elif mode == 2:
 
         f.write("0 "+str(mid_point)+"\n")
         f.write(str(width)+"\n");
         for i in range(width):
             height = int(random.uniform(0, 1)*width/4+width*3/4)
-            f.write(str(height)+" "+str(i)+"\n")
+            f.write(str(height-1)+" "+str(i)+"\n")
     elif mode == 3:
         lightning_col = lightning_width
         for i in range(lightning_point):
@@ -36,6 +36,7 @@ with open(output_file, 'w') as f:
             lightning_col += lightning_width
         f.write(str(width)+"\n");
         for i in range(width):
-            height = int(random.uniform(0, 1)*width/4+width*3/4)
+            height = int(random.uniform(0, 1)*width/5+width*4/5)
+            height = max(height, width-1)
             f.write(str(height)+" "+str(i)+"\n")
     
